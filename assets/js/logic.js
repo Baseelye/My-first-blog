6d6c16f778120f5aae5c3982dc28c36a0e177722
Baseelye <165911977+Baseelye@users.noreply.github.com>
2024-05-13 12:52:33 -0400

    blog post data stored to local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,12 @@
 let formEl = document.getElementById("submitting");
 console.log(formEl);
+let listBlog = []
+
+let insideLocal = JSON.parse(localStorage.getItem("Blog"));
+console.log(insideLocal);
+if(insideLocal) {
+    listBlog = insideLocal
+}
 
 formEl.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -11,4 +18,12 @@ if (!username || title || content) {
     window.alert ('You need to complete the form');
     return;
 }
+let inputData = {
+    username, title, content
+}
+listBlog.push(inputData);
+console.log(listBlog);
+
+localStorage.setItem("Blogs", JSON.stringify(listBlog));
+
 });
